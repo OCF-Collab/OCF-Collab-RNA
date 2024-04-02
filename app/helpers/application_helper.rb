0f169615bd5df46bd709ec91342537f0f077f6e3
Alex Nizamov <nizamov.alex@gmail.com>
2024-04-02 18:29:35 +0500

    Update landing page text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def external_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options ||= {}
+    link_to(name, options, html_options.merge(target: "_blank"), &block)
+  end
 end
